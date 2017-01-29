7fa865643b5f721b11e24b06ff9a94a01ee76e42
emlai <emil@cs.helsinki.fi>
2017-01-29 13:05:33 +0200

    allow leading underscore in identifiers

--- src/parser/lexer.cpp
+++ src/parser/lexer.cpp
@@ -220,7 +220,7 @@ int lex() {
             case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
             case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
             case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
-            case 'v': case 'w': case 'x': case 'y': case 'z': {
+            case 'v': case 'w': case 'x': case 'y': case 'z': case '_': {
                 std::string string;
                 do {
                     string += (char) ch;
