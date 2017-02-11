fcc92cf5633589068313897d39036686ec32e875
emlai <emil@cs.helsinki.fi>
2017-02-11 18:57:53 +0200

    Fix error() if srcLoc.file is null or !srcLoc.isValid()

--- src/driver/utility.h
+++ src/driver/utility.h
@@ -9,20 +9,27 @@ inline std::ostream& operator<<(std::ostream& stream, llvm::StringRef string) {
 
 template<typename... Args>
 [[noreturn]] inline void error(SrcLoc srcLoc, Args&&... args) {
-    std::cout << srcLoc.file << ':';
-    if (srcLoc.isValid()) std::cout << srcLoc.line << ':' << srcLoc.column << ':';
+    if (srcLoc.file) {
+        std::cout << srcLoc.file << ':';
+        if (srcLoc.isValid()) std::cout << srcLoc.line << ':' << srcLoc.column << ':';
+    } else {
+        std::cout << "<unknown file>:";
+    }
+    
     std::cout << " error: ";
     using expander = int[];
     (void)expander{0, (void(std::cout << std::forward<Args>(args)), 0)...};
 
-    // Output caret.
-    std::ifstream file(srcLoc.file);
-    while (--srcLoc.line) file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::string line;
-    std::getline(file, line);
-    std::cout << '\n' << line << '\n';
-    while (--srcLoc.column) std::cout << ' ';
-    std::cout << "^\n";
+    if (srcLoc.file && srcLoc.isValid()) {
+        // Output caret.
+        std::ifstream file(srcLoc.file);
+        while (--srcLoc.line) file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::string line;
+        std::getline(file, line);
+        std::cout << '\n' << line << '\n';
+        while (--srcLoc.column) std::cout << ' ';
+        std::cout << "^\n";
+    }
 
     exit(1);
 }
