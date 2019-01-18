68c3810ed554b3658f96f208d6c8d8023be953fc
emlai <emil.laine@helsinki.fi>
2019-01-18 23:33:49 +0200

    Test that examples compile

---
+++ examples/build_examples.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+delta=${1:-delta}
+
+cd "$(dirname "$0")"
+
+for file in ./*.delta; do
+    output="${file%.delta}.out"
+    $delta $file -o$output
+done
+
+echo "All examples built successfully."
