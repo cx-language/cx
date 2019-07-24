636f427699fdcb243b241de8031597839213512f
Emil Laine <emil.laine@helsinki.fi>
2019-07-24 23:09:44 +0300

    Update sandbox version file path

--- scripts/update-delta-sandbox.sh
+++ scripts/update-delta-sandbox.sh
@@ -8,8 +8,8 @@ git clone https://github.com/delta-lang/delta-lang.github.io.git
 cd delta-lang.github.io
 
 DELTA_VERSION=$TRAVIS_COMMIT
-echo $DELTA_VERSION > src-backend/.delta-version
-git add src-backend/.delta-version
+echo $DELTA_VERSION > server/.delta-version
+git add server/.delta-version
 git commit --message "Update Delta version to $DELTA_VERSION" --author "Delta CI <>"
 
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/delta-lang/delta-lang.github.io.git > /dev/null 2>&1
