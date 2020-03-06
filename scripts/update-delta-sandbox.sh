e0c4e40bca0fa0ba0e3f7e345335eb7bc25c222b
Emil Laine <laine.emil@gmail.com>
2020-03-06 22:11:28 +0200

    Amend consecutive sandbox update commits
    for cleaner history

--- scripts/update-delta-sandbox.sh
+++ scripts/update-delta-sandbox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $TRAVIS_REPO_SLUG != "delta-lang/delta" ] || [ $TRAVIS_BRANCH != "master" ]; then
-    exit;
+    exit
 fi
 
 git clone https://github.com/delta-lang/delta-lang.github.io.git
@@ -10,7 +10,13 @@ cd delta-lang.github.io
 DELTA_VERSION=$TRAVIS_COMMIT
 echo $DELTA_VERSION > server/.delta-version
 git add server/.delta-version
-git commit --message "Update Delta version to $DELTA_VERSION" --author "Delta CI <>"
+
+PREVIOUS_COMMIT_AUTHOR=$(git log -1 --pretty=format:%an)
+if [ $PREVIOUS_COMMIT_AUTHOR == "Delta CI" ]; then
+    AMEND=--amend
+fi
+
+git commit --message "Update Delta version to $DELTA_VERSION" --author "Delta CI <>" $AMEND
 
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/delta-lang/delta-lang.github.io.git > /dev/null 2>&1
-git push --quiet
+git push --quiet --force-with-lease
