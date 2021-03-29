f3b2d7aadd00dbeb48b19e6bdeed22eb6ab3af41
Emil Laine <laine.emil@gmail.com>
2021-03-29 21:32:03 +0300

    Make -print-* compiler options work under the 'build' command

--- src/driver/driver.cpp
+++ src/driver/driver.cpp
@@ -52,9 +52,9 @@ cl::list<std::string> inputs(cl::Positional, cl::desc("<input files>"), cl::sub(
 cl::opt<bool> parse("parse", cl::desc("Parse only"));
 cl::opt<bool> typecheck("typecheck", cl::desc("Parse and type-check only"));
 cl::opt<bool> compileOnly("c", cl::desc("Compile only, generating an object file; don't link"));
-cl::opt<bool> printIR("print-ir", cl::desc("Print Delta intermediate representation of main module"));
-cl::opt<bool> printIRAll("print-ir-all", cl::desc("Print Delta intermediate representation of all compiled modules"));
-cl::opt<bool> printLLVM("print-llvm", cl::desc("Print LLVM intermediate representation of main module"));
+cl::opt<bool> printIR("print-ir", cl::desc("Print Delta intermediate representation of main module"), cl::sub(build), cl::sub(*cl::TopLevelSubCommand));
+cl::opt<bool> printIRAll("print-ir-all", cl::desc("Print Delta intermediate representation of all compiled modules"), cl::sub(build), cl::sub(*cl::TopLevelSubCommand));
+cl::opt<bool> printLLVM("print-llvm", cl::desc("Print LLVM intermediate representation of main module"), cl::sub(build), cl::sub(*cl::TopLevelSubCommand));
 // TODO: Add -print-llvm-all option.
 cl::opt<bool> emitAssembly("emit-assembly", cl::desc("Emit assembly code"));
 cl::opt<bool> emitBitcode("emit-llvm-bitcode", cl::desc("Emit LLVM bitcode"));
