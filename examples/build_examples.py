e092d542f7df589eb9b28acf744655a187e18902
Emil Laine <laine.emil@gmail.com>
2021-04-09 19:36:01 +0300

    Add recursive directory listing example program

--- examples/build_examples.py
+++ examples/build_examples.py
@@ -11,6 +11,9 @@ cx_path = sys.argv[1] if len(sys.argv) > 1 else "cx"
 os.chdir(os.path.dirname(__file__))
 
 for file in glob.glob("*.cx"):
+    if platform.system() == "Windows" and file == "tree.cx":
+        continue
+
     extension = ".out" if platform.system() != "Windows" else ".exe"
     output = os.path.splitext(file)[0] + extension
     exit_status = subprocess.call([cx_path, file, "-o", output])
