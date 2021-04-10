b512d7b70cb8699728324e85cf62bee34d10316d
Emil Laine <laine.emil@gmail.com>
2021-04-10 16:25:52 +0300

    Use 3-state result type to avoid null analysis false positives

--- src/sema/null-analyzer.h
+++ src/sema/null-analyzer.h
@@ -16,17 +16,16 @@ struct BasicBlock;
 struct Value;
 struct Instruction;
 
+enum class Nullability { DefinitelyNullable, IndefiniteNullability, DefinitelyNotNull };
+
 struct NullAnalyzer {
     llvm::SmallPtrSet<BasicBlock*, 16> visited;
 
     void analyze(IRModule* module);
     void analyze(Value* value);
-    /// Returns true if the given expression (of optional type) is guaranteed to be non-null, e.g.
-    /// if it was previously checked against null, and the type-checker can prove that it wasn't set
-    /// back to null after that check.
-    bool isDefinitelyNotNull(Value* nullableValue, Instruction* startFrom);
-    bool isDefinitelyNotNullRecursive(Value* nullableValue, Instruction* startFrom, int gepIndex = -1);
-    bool isNotNullWhenComingFrom(Value* nullableValue, BasicBlock* predecessor, BasicBlock* destination, int gepIndex);
+    Nullability analyzeNullability(Value* nullableValue, Instruction* startFrom);
+    Nullability analyzeNullability_recursive(Value* nullableValue, Instruction* startFrom, int gepIndex = -1);
+    Nullability analyzeNullability_fromPredecessor(Value* nullableValue, BasicBlock* predecessor, BasicBlock* destination, int gepIndex);
 };
 
 } // namespace cx
