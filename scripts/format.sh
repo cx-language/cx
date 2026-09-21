6a1c0f78a003664630a25f7f92d0587ac323152c
Emil Laine <laine.emil@gmail.com>
2026-09-21 03:20:02 +0300

    Remove clang-tidy from the project

--- scripts/format.sh
+++ scripts/format.sh
@@ -10,11 +10,7 @@ while test $# -gt 0; do
     case "$1" in
         --check) CHECK=1
             ;;
-        --skip-clang-tidy) SKIP_CLANG_TIDY=1
-            ;;
         --*) echo "bad option $1"
-            ;;
-        *) BUILD_PATH="$1"
             ;;
     esac
     shift
@@ -27,23 +23,14 @@ check_version() {
     fi
 }
 
-run_clang_tidy() {
-    if [ -n "$SKIP_CLANG_TIDY" ]; then
-        return 0
-    fi
-    python3 "$BUILD_PATH/run-clang-tidy.py" -header-filter="^$ROOTDIR/src/.*" -quiet "$@" $FILES 2>&1 \
-        | sed -E '/^($|clang-tidy|[0-9]+ warnings generated)/d'
-}
-
 run_clang_format() {
     ! (clang-format -output-replacements-xml $FILES | grep "<replacement " >/dev/null)
 }
 
 check_version clang-format
-check_version clang-tidy
 
 if [ -n "$CHECK" ]; then
-    run_clang_tidy && run_clang_format
+    run_clang_format
 
     if [ $? -ne 0 ]; then
         echo "Run the 'format' target to format the code."
@@ -53,5 +40,4 @@ if [ -n "$CHECK" ]; then
     fi
 fi
 
-run_clang_tidy -fix -format
 clang-format -i $FILES
