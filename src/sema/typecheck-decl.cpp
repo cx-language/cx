6776148cb8a5cf557468e3e896178c553fc5ebf6
Emil Laine <laine.emil@gmail.com>
2026-09-23 20:10:12 +0300

    Clarify generic parameter diagnostic

--- src/sema/typecheck-decl.cpp
+++ src/sema/typecheck-decl.cpp
@@ -314,7 +314,7 @@ void Typechecker::typecheckGenericParamDecls(llvm::ArrayRef<GenericParamDecl> ge
                 typecheckType(genericParam.valueType, userAccessLevel);
 
                 if (errors == errorsBefore && !genericParam.valueType.isInteger()) {
-                    ERROR(genericParam.valueType.location, "integer generic parameter '" << genericParam.getName() << "' must have integer type");
+                    ERROR(genericParam.valueType.location, "generic parameters must be types or integers");
                 }
             } catch (const CompileError& error) {
                 error.report();
