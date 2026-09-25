04d2689fb0bb243abe1705f2d26de71ae7f74df8
Emil Laine <laine.emil@gmail.com>
2026-09-25 12:28:31 +0300

    Trim precompiled header to broadly-used lightweight headers

--- src/pch.h
+++ src/pch.h
@@ -1,18 +1,20 @@
 #pragma once
 
+// Precompiled header: only broadly-used, lightweight headers belong here.
+// Heavy, narrowly-used headers (all of Clang, LLVM IR/Pass/Target/CodeGen,
+// <fstream>, <sstream>, ...) must be included directly by the few TUs that
+// need them, so the other batches don't pay to deserialize them.
+
 #include <algorithm>
 #include <cassert>
 #include <cctype>
 #include <cerrno>
 #include <cstdio>
-#include <forward_list>
-#include <fstream>
 #include <functional>
 #include <iosfwd>
 #include <limits>
 #include <memory>
 #include <ostream>
-#include <sstream>
 #include <string>
 #include <system_error>
 #include <unordered_map>
@@ -20,57 +22,24 @@
 #include <vector>
 
 #pragma warning(push, 0)
-#include <clang/AST/Decl.h>
-#include <clang/AST/DeclGroup.h>
-#include <clang/AST/PrettyPrinter.h>
-#include <clang/AST/Type.h>
-#include <clang/Basic/Builtins.h>
-#include <clang/Basic/Diagnostic.h>
-#include <clang/Basic/DiagnosticIDs.h>
-#include <clang/Basic/TargetInfo.h>
-#include <clang/Driver/Compilation.h>
-#include <clang/Driver/Driver.h>
-#include <clang/Frontend/CompilerInstance.h>
-#include <clang/Frontend/TextDiagnosticPrinter.h>
-#include <clang/Lex/HeaderSearch.h>
-#include <clang/Lex/Preprocessor.h>
-#include <clang/Lex/PreprocessorOptions.h>
-#include <clang/Parse/ParseAST.h>
-#include <clang/Sema/Sema.h>
 #include <llvm/ADT/APFloat.h>
 #include <llvm/ADT/APSInt.h>
 #include <llvm/ADT/ArrayRef.h>
 #include <llvm/ADT/DenseMap.h>
 #include <llvm/ADT/SmallPtrSet.h>
 #include <llvm/ADT/SmallVector.h>
-#include <llvm/ADT/StringExtras.h>
 #include <llvm/ADT/StringMap.h>
 #include <llvm/ADT/StringRef.h>
 #include <llvm/ADT/StringSet.h>
 #include <llvm/ADT/StringSwitch.h>
-#include <llvm/Bitcode/BitcodeWriter.h>
-#include <llvm/IR/CFG.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/Verifier.h>
-#include <llvm/Linker/Linker.h>
 #include <llvm/Support/Casting.h>
-#include <llvm/Support/CodeGen.h>
-#include <llvm/Support/CommandLine.h>
 #include <llvm/Support/ErrorHandling.h>
 #include <llvm/Support/ErrorOr.h>
 #include <llvm/Support/FileSystem.h>
-#include <llvm/Support/InitLLVM.h>
 #include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Support/Path.h>
 #include <llvm/Support/Process.h>
 #include <llvm/Support/Program.h>
 #include <llvm/Support/SaveAndRestore.h>
-#include <llvm/Support/Signals.h>
-#include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/VirtualFileSystem.h>
 #include <llvm/Support/raw_ostream.h>
-#include <llvm/Target/TargetMachine.h>
-#include <llvm/TargetParser/Host.h>
 #pragma warning(pop)
